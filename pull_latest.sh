3807255d75e48f44b9860c939b823af3b9f09fac
mehrain <marijnrovers@live.nl>
2024-08-06 22:41:43 +0200

    updated pull latest to include pip

--- pull_latest.sh
+++ pull_latest.sh
@@ -3,8 +3,12 @@
 # Navigate to the application directory
 cd /apps/Pi5Bot
 
-# Pull the latest changes from the repository
-git pull origin main
+# Force pull the latest changes from the repository
+git fetch --all
+git reset --hard origin/main
+
+# Install dependencies
+pip install --no-cache-dir -r requirements.txt
 
 # Continue with the rest of the startup process
 exec "$@"
