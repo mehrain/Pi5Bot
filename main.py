15b1e5468c6bf7b8c6354d48e3097b928c67c8e7
mehrain <marijnrovers@live.nl>
2024-08-06 22:46:39 +0200

    testing if bot selfupdates

--- main.py
+++ main.py
@@ -25,6 +25,9 @@ async def on_message(message):
     if message.content.startswith('$bye'):
         await message.channel.send('Goodbye!')
         
+    if message.content.startswith('$help'):
+        await message.channel.send('Hello! I am a bot. I can help you with the following commands: \n $hello - To greet you \n $bye - To say goodbye \n $help - To display this message')
+        
 #fetching the token 
 token = os.getenv('DISCORD_TOKEN')  
 
