58969f8b2ba74019e0f288c5ffbaa9f3e9d1b6c9
mehrain <marijnrovers@live.nl>
2024-08-10 22:54:38 +0200

    added base recent lookups and will implement targetted lookup aswell.

--- src/cogs/bootdev.py
+++ src/cogs/bootdev.py
@@ -6,28 +6,54 @@ from src.functions.bootdevparse.Pokematch import Pokematch
 class BootDev(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.df = None
         #print ("BootDev cog initialized")
 
     @commands.command()
-    async def arch10(self, ctx):
+    async def archrecent(self, ctx, number: int):
+        
         try:
             # Read the Pokemon CSV file
             df = pd.read_csv('BDparsed_with_pokemon.csv')
             print("CSV file read successfully")
-
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            await ctx.send("An error occurred while building dataframe. That sucks.")
+            return
+        
+        if not number:
+            await ctx.send("Please enter a number")
+            return
+        elif number <= 0:
+            await ctx.send("Please enter a number greater than 0")
+            return
+        elif number > len(df):
+            await ctx.send("Number exceeds the length of the CSV file")
+            return
+        
+        try:
             # Get the last 10 rows
-            last_10_rows = df.tail(10)
-            print("Last 10 rows extracted")
+            last_x_rows = df.tail(number)
+            print(f"Last {number} rows extracted")
+            
+            total_characters = last_x_rows.astype(str).sum(axis=1).str.len().sum()
+            if total_characters > 6000:
+                await ctx.send("The total number of characters exceeds the limit of 6000.")
+                return
 
             # Send the last 10 rows as an embed message
-            embed = discord.Embed(title="Last 10 Archmages", color=discord.Color.gold())
-            for index, row in last_10_rows.iterrows():
+            embed = discord.Embed(title=f"Last {number} Archmages", color=discord.Color.gold())
+            for index, row in last_x_rows.iterrows():
                 embed.add_field(name=f"Archmage rank: {index+1}", value=row.to_string(index=False), inline=False)
             await ctx.send(embed=embed)
             print("Embed message sent successfully")
         except Exception as e:
             print(f"An error occurred: {e}")
-            await ctx.send("An error occurred. That sucks.")
+            await ctx.send("An error occurred while sending the embed message. That sucks.")
+            
+    @commands.command()
+    async def archlookup(self, ctx, what :str, value):
+        pass
             
     @commands.command()
     async def archsync(self, ctx):
@@ -43,9 +69,15 @@ class BootDev(commands.Cog):
             result = pokematch_instance.run()
             print("Pokematch run method executed")
             
+            # Send a nice embed message to indicate successful completion of Pokematch run
+            embed = discord.Embed(title="Pokematch Run Completed", color=discord.Color.green())
+            embed.add_field(name="Status", value="Pokematch run completed successfully", inline=False)
+            await ctx.send(embed=embed)
+            
         except Exception as e:
             print(f"An error occurred: {e}")
             await ctx.send("An error occurred. That sucks.")
+            
             
   
   # make the Pokematch run every hour          
@@ -76,5 +108,7 @@ def setup(bot):
     boot_dev_instance = BootDev(bot)
     bot.add_cog(boot_dev_instance)
     boot_dev_instance.start_scheduler()
+    boot_dev_instance.archsync()
+    
     
 
