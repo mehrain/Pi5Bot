91012d78c20d4c129d345015e711544d72027578
mehrain <marijnrovers@live.nl>
2024-08-10 20:44:55 +0200

    Gonna work on cronjobs

--- src/cogs/bootdev.py
+++ src/cogs/bootdev.py
@@ -11,17 +11,6 @@ class BootDev(commands.Cog):
     @commands.command()
     async def last10(self, ctx):
         try:
-            # Log the start of the command
-            print("pokematch command started")
-
-            # Instantiate the Pokematch class
-            pokematch_instance = Pokematch()
-            print("Pokematch instance created")
-
-            # Execute the run method
-            result = pokematch_instance.run()
-            print("Pokematch run method executed")
-
             # Read the Pokemon CSV file
             df = pd.read_csv('BDparsed_with_pokemon.csv')
             print("CSV file read successfully")
@@ -36,6 +25,24 @@ class BootDev(commands.Cog):
                 embed.add_field(name=f"Archmage rank: {index+1}", value=row.to_string(index=False), inline=False)
             await ctx.send(embed=embed)
             print("Embed message sent successfully")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            await ctx.send("An error occurred. That sucks.")
+            
+    @commands.command()
+    async def archsync(self, ctx):
+        try:
+            # Log the start of the command
+            print("pokematch command started")
+
+            # Instantiate the Pokematch class
+            pokematch_instance = Pokematch()
+            print("Pokematch instance created")
+
+            # Execute the run method
+            result = pokematch_instance.run()
+            print("Pokematch run method executed")
+            
         except Exception as e:
             print(f"An error occurred: {e}")
             await ctx.send("An error occurred. That sucks.")
